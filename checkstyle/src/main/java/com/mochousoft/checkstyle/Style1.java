d8ce8dbb629b25cfec76f41d04f994ed0dcf436f
fushuwei <331044761@qq.com>
2022-09-11 18:17:52 +0800

    增加checkstyle静态语法校验模块

---
+++ checkstyle/src/main/java/com/mochousoft/checkstyle/Style1.java
@@ -0,0 +1,14 @@
+package com.mochousoft.checkstyle;
+
+
+
+
+public class Style1 {
+    public Style1()
+    {
+        System.out.println( 1 ) ;
+
+    }
+
+
+}
---
+++ checkstyle/src/test/java/com/mochousoft/checkstyle/Style1Test.java
@@ -0,0 +1,7 @@
+package com.mochousoft.checkstyle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Style1Test {
+
+}
