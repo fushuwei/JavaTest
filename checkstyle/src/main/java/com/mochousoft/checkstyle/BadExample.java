3bb4e4d04006022fc9f4935afafb5a949937bd0e
fushuwei <331044761@qq.com>
2022-09-13 23:47:48 +0800

    完善 checkstyle 配置, 并列举一些场景的违规示例

--- checkstyle/src/main/java/com/mochousoft/checkstyle/BadExample.java
+++ checkstyle/src/main/java/com/mochousoft/checkstyle/BadExample.java
@@ -1,33 +1,51 @@
 package com.mochousoft.checkstyle;
 
 
+public class BadExample { // 1、没有类的 Javadoc，2、有两个空行
 
+    private static final String var_constant = "BadExample"; // 常量命名不规范
 
-public class BadExample {
-    public static final long abc = 100;
-    public BadExample()
-    {
-        System.out.println( 1 ) ;
+    private final static String VAR_CONSTANT = "BadExample"; // static 和 final 的顺序错误
 
-        String a = "abc";
-        try{
-            System.out.print(123);
-            System.out.println(100);
-        }catch(Exception e ){
+    private static String var_static; // 静态变量命名不规范
+
+    String name;
+
+    public int getAge() { // 符合规范，CheckStyle 将其识别为 getter setter 方法
+        return 18;
+    }
+
+    public int getage() { // 是普通方法，但是没有方法的 Javadoc
+        return 18;
+    }
+
+    @Override
+    public String toString() { // 因为使用了 @Override 注解，所以可以没有方法的 Javadoc
+        return super.toString();
+    }
+
+    /**
+     *
+     */
+    public int test(int a, int b) { // 方法的 Javadoc 中允许没有 @param 和 @return
+        // @formatter:off
+        int c = a +b; // + 号后面没有空格
+        // @formatter:on
+
+        try {
+            System.out.println(c);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        // TODO admin
 
-
+        return c;
     }
 
-
-    public void test () {
-
+    public String getName() {
+        return name;
     }
 
-    public int getTest() {
-        return 1;
+    public void setName(String name) {
+        this.name = name;
     }
-
 }
