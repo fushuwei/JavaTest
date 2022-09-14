2022ffe1a8da194918151850bed28fa306d3d4c3
fushuwei <331044761@qq.com>
2022-09-15 00:43:15 +0800

    测试 Spring Data JPA 基础功能

---
+++ spring-data-jpa/src/main/java/com/mochousoft/springdata/jpa/entity/User.java
@@ -0,0 +1,22 @@
+package com.mochousoft.springdata.jpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "t_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
--- spring-data-jpa/src/test/java/com/mochousoft/springdata/jpa/SpringDataJpaApplicationTests.java
+++ spring-data-jpa/src/test/java/com/mochousoft/springdata/jpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mochousoft.springdata.jpa;
 
+import com.mochousoft.springdata.jpa.entity.User;
+import com.mochousoft.springdata.jpa.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void list() {
+        List<User> userList = userRepository.findAll();
+        for (User user : userList) {
+            System.out.println(user.getId());
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword() + "\n\n");
+        }
+    }
+
+    @Test
+    void page() {
+        userRepository.findAll(PageRequest.of(1, 1)).forEach(System.out::println);
+    }
 
 }
